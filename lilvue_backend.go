32dd80933eb2f6d7f7205ae84ba0336c47e88e23
Max Wittmann <damxam@gmail.com>
2019-04-21 02:33:28 +1000

    ...

--- lilvue_backend.go
+++ lilvue_backend.go
@@ -1,10 +1,11 @@
 package main
 
 import (
-	"fmt"
+	// "fmt"
 	"io/ioutil"
 	"log"
 	"net/http"
+	"html/template"
 )
 
 type Page struct {
@@ -51,13 +52,58 @@ func loadPage(title string) (*Page, error) {
 //     fmt.Fprintf(w, "Hi there, I love %s!", r.URL.Path[1:])
 // }
 
+func renderTemplate(w http.ResponseWriter, tmpl string, p *Page) {
+	print(">>>>>>>>>>>> " + tmpl + ".html")
+	t, _ := template.ParseFiles(tmpl + ".html")
+	
+    t.Execute(w, p)
+}
+
 func viewHandler(w http.ResponseWriter, r *http.Request) {
 	title := r.URL.Path[len("/view/"):]
-	p, _ := loadPage(title)
-	fmt.Fprintf(w, "<h1>%s</h1><div>%s</div>", p.Title, p.Body)
+	p, err := loadPage(title)
+
+	if err != nil {
+        http.Redirect(w, r, "/edit/"+title, http.StatusFound)
+        return
+	}
+	
+	// fmt.Fprintf(w, "<h1>%s</h1><div>%s</div>", p.Title, p.Body)
+
+	// t, _ := template.ParseFiles("view.html")
+
+	renderTemplate(w, "view", p)
+
+    // t.Execute(w, p)
+}
+
+func editHandler(w http.ResponseWriter, r *http.Request) {
+	title := r.URL.Path[len("/edit"):]
+	p, err := loadPage(title)
+	if err != nil {
+		p = &Page{Title: title}
+	}
+	// fmt.Fprintf(w, "<h1>Editing %s</h1>" + 
+	// 	"<form action=\"	
+	// )
+
+	// t, _ := template.ParseFiles("edit.html")
+	// t.Execute(w, p)
+	renderTemplate(w, "edit", p)
+}
+
+func saveHandler(w http.ResponseWriter, r *http.Request) {
+	title := r.URL.Path[len("/save/"):]
+	body := r.FormValue("body")
+	print("body val: " + body)
+	p := &Page{Title: title, Body: []byte(body)}
+	p.save()
+	http.Redirect(w, r, "/view/" + title, http.StatusFound)
 }
 
 func main() {
     http.HandleFunc("/view/", viewHandler)
+    http.HandleFunc("/edit/", editHandler)
+    http.HandleFunc("/save/", saveHandler)
     log.Fatal(http.ListenAndServe(":8080", nil))
 }
