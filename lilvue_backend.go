e1f347331e570bdc24620b99032af0a836b5fcd8
Max Wittmann <damxam@gmail.com>
2019-04-21 02:34:38 +1000

    cleanup

--- lilvue_backend.go
+++ lilvue_backend.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	// "fmt"
 	"io/ioutil"
 	"log"
 	"net/http"
@@ -11,20 +10,6 @@ import (
 type Page struct {
     Title string
     Body  []byte
-
-	// func (p *Page) save() error {
-	// 	filename := p.Title + ".txt"
-	// 	return ioutil.WriteFile(filename, p.Body, 0600)
-	// }
-	
-	// func loadPage(title string) (*Page, error) {
-	// 	filename := title + ".txt"
-	// 	body, err := ioutil.ReadFile(filename)
-	// 	if err != nil {
-	// 		return nil, err
-	// 	}
-	// 	return &Page{Title: title, Body: body}, nil
-	// }
 }
 
 func (p *Page) save() error {
@@ -40,17 +25,6 @@ func loadPage(title string) (*Page, error) {
 	}
 	return &Page{Title: title, Body: body}, nil
 }
-
-// func main() {
-//     p1 := &Page{Title: "TestPage", Body: []byte("This is a sample Page.")}
-//     p1.save()
-//     p2, _ := loadPage("TestPage")
-//     fmt.Println(string(p2.Body))
-// }
-
-// func handler(w http.ResponseWriter, r *http.Request) {
-//     fmt.Fprintf(w, "Hi there, I love %s!", r.URL.Path[1:])
-// }
 
 func renderTemplate(w http.ResponseWriter, tmpl string, p *Page) {
 	print(">>>>>>>>>>>> " + tmpl + ".html")
@@ -68,13 +42,7 @@ func viewHandler(w http.ResponseWriter, r *http.Request) {
         return
 	}
 	
-	// fmt.Fprintf(w, "<h1>%s</h1><div>%s</div>", p.Title, p.Body)
-
-	// t, _ := template.ParseFiles("view.html")
-
 	renderTemplate(w, "view", p)
-
-    // t.Execute(w, p)
 }
 
 func editHandler(w http.ResponseWriter, r *http.Request) {
@@ -83,12 +51,6 @@ func editHandler(w http.ResponseWriter, r *http.Request) {
 	if err != nil {
 		p = &Page{Title: title}
 	}
-	// fmt.Fprintf(w, "<h1>Editing %s</h1>" + 
-	// 	"<form action=\"	
-	// )
-
-	// t, _ := template.ParseFiles("edit.html")
-	// t.Execute(w, p)
 	renderTemplate(w, "edit", p)
 }
 
