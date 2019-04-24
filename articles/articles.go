0b96969cdcc10d9db472b8de4e92fd8f101da731
Max Wittmann <damxam@gmail.com>
2019-04-24 10:43:20 +1000

    Json response now

--- articles/articles.go
+++ articles/articles.go
@@ -1,13 +1,61 @@
 package articles
 
 import (
-	"fmt"
+	// "fmt"
 	"net/http"
+	"encoding/json"
 	"github.com/gorilla/mux"
 )
 
+// package main
+
+// import (
+//   "encoding/json"
+//   "net/http"
+// )
+
+// type Profile struct {
+//   Name    string
+//   Hobbies []string
+// }
+
+// func main() {
+//   http.HandleFunc("/", foo)
+//   http.ListenAndServe(":3000", nil)
+// }
+
+// func foo(w http.ResponseWriter, r *http.Request) {
+//   profile := Profile{"Alex", []string{"snowboarding", "programming"}}
+
+//   js, err := json.Marshal(profile)
+//   if err != nil {
+//     http.Error(w, err.Error(), http.StatusInternalServerError)
+//     return
+//   }
+
+//   w.Header().Set("Content-Type", "application/json")
+//   w.Write(js)
+// }
+
+type Profile struct {
+	Name    string
+	Hobbies []string
+  }
+  
 func ArticlesCategoryHandler(w http.ResponseWriter, r *http.Request) {
     vars := mux.Vars(r)
-    w.WriteHeader(http.StatusOK)
-    fmt.Fprintf(w, "Category: %v\n", vars["category"])
+    // w.WriteHeader(http.StatusOK)
+	// // fmt.Fprintf(w, "Category: %v\n", vars["category"])
+	// fmt.Fprintf(w, "{ 'category': '%v' }", vars["category"])
+
+	profile := Profile{"Alex", []string{ vars["category"], "programming"}}
+
+	js, err := json.Marshal(profile)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(js)
 }
